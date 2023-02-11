43a27f290d62d6424a10b654acae3da5ee52b0d6
Parkseungje1 <tmdwprpdla+1@gmail.com>
2023-02-11 16:39:35 +0900

    init project

---
+++ src/main/kotlin/com/example/realworldkotlin/RealworldKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.realworldkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealworldKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<RealworldKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/realworldkotlin/RealworldKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.realworldkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RealworldKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
